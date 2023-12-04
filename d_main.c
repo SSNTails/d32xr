bf081fb95f2bf6991fe935d418c8bcca34fc5ec0
viciious <vluchits@gmail.com>
2023-12-04 11:03:56 +0300

    No SSF

--- d_main.c
+++ d_main.c
@@ -493,8 +493,8 @@ int TIC_Abortable (void)
 
 	if (ticon < TICVBLS)
 		return 0;
-	if (ticon >= gameinfo.titleTime)
-		return 1;		/* go on to next demo */
+	//if (ticon >= gameinfo.titleTime)
+	//	return 1;		/* go on to next demo */
 
 #ifdef JAGUAR	
 	if (ticbuttons[0] == (BT_OPTION|BT_STAR|BT_HASH) )
